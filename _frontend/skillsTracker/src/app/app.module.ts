265b5b6cf0426bbc6c8307106b582718a83f5f19
joe61081 <joe61081@mastek.com>
2019-01-29 14:42:21 +0000

    Generated Role Component

--- _frontend/skillsTracker/src/app/app.module.ts
+++ _frontend/skillsTracker/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoleComponent } from './role/role.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoleComponent
   ],
   imports: [
     BrowserModule,
