315f67be4bbb6a80e1d5dcffcf2ed63364acf625
joe61081 <joe61081@mastek.com>
2019-01-29 11:37:58 +0000

    Added methods to allow calls to retrieve data from database

--- backend/valuetracker/src/main/java/com/mastek/valuetracker/RoleAccessAPI.java
+++ backend/valuetracker/src/main/java/com/mastek/valuetracker/RoleAccessAPI.java
@@ -1,14 +1,21 @@
 package com.mastek.valuetracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@Path("/Role/")
-public class RoleAccessAPI {
+@Path("/roles/")
+public class RoleAccessAPI{
     RoleJPARepository repository;
     public RoleJPARepository getRepository() {
         return repository;
@@ -18,8 +25,29 @@ public class RoleAccessAPI {
         this.repository = repository;
     }
     
-    public Role findById(int id) {
+    @Path("/list")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Iterable<Role> listRoles(){
+		return getRepository().findAll();
+	}
+    
+    @Path("/findById")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Role findById(@QueryParam("id") int id) {
         return getRepository().findById(id).get(); 
+    }
+    
+    @Path("/findByRole")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Role> findByRole(@QueryParam("skill") String skill) {
+    	List<Role> roleList = new ArrayList<Role>();
+    	for (Role roles : getRepository().findByRole(skill)) {
+    		roleList.add(roles);
+		}
+        return roleList; 
     }
     
     public Role add(Role i) {
--- backend/valuetracker/src/main/java/com/mastek/valuetracker/RoleJPARepository.java
+++ backend/valuetracker/src/main/java/com/mastek/valuetracker/RoleJPARepository.java
@@ -1,8 +1,15 @@
 package com.mastek.valuetracker;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface RoleJPARepository extends CrudRepository<Role,Integer>{
+	
+	 @Query(value = "SELECT * FROM `roles` t WHERE t.skill LIKE CONCAT('%',:skill, '%')",nativeQuery = true)
+	    public List<Role> findByRole(@Param("skill")String skill);
 }
