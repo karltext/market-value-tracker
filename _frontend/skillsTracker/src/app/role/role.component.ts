dbc7e5cbdfe3519bc2d92b386fa0fa5de1ba1139
joe61081 <joe61081@mastek.com>
2019-01-30 10:21:29 +0000

    Added code to retrieve Role by Name

--- _frontend/skillsTracker/src/app/role/role.component.ts
+++ _frontend/skillsTracker/src/app/role/role.component.ts
@@ -10,6 +10,7 @@ import { Role } from '../role';
 export class RoleComponent implements OnInit {
 
   roles: Role[]
+  fltRoles: Role[]
 
   constructor(private roleService: RoleService) { }
 
@@ -21,6 +22,14 @@ export class RoleComponent implements OnInit {
     this.roleService.getRolesList()
       .subscribe(res => {
         this.roles = res.slice(0, 100)
+      })
+  }
+
+  getRolesByName(role:String) {
+    this.roleService.getRolesByName(role)
+      .subscribe(res => {
+        console.log(this.fltRoles)
+        this.fltRoles = res
       })
   }
 
