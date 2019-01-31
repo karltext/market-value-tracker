5de189bfd6c687212cafa59f08370e0624bfaa34
karltext <44974965+karltext@users.noreply.github.com>
2019-01-31 13:45:02 +0000

    basic profile working

--- _frontend/skillsTracker/src/app/profile.service.ts
+++ _frontend/skillsTracker/src/app/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Profile } from './profile';
 import { Skill } from './Skill';
 import { Role } from './role';
@@ -15,6 +15,11 @@ export class ProfileService {
 
   rootURL = "http://localhost:9900/profiles"
 
+  postOptions = {
+    headers: new HttpHeaders(
+      {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' })
+  }
+
   getProfile(profileId: number): Observable<Profile> {
     const url = this.rootURL + "/find?profileId=" + profileId
     return this.http.get<Profile>(url)
@@ -23,5 +28,11 @@ export class ProfileService {
   getProfileRoles(profileId: number): Observable<Role[]> {
     const url = this.rootURL + "/" + profileId + "/roles"
     return this.http.get<Role[]>(url)
+  }
+
+  addRoleToProfile(profileId: number, roleId: number): Observable<Role> {
+    const url = this.rootURL + "/role/add"
+    const body = "profileId=" + profileId + "&roleId=" + roleId
+    return this.http.post<Role>(url, body, this.postOptions)
   }
 }
